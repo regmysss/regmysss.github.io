bdd68b4d0b2f519a5744e901395300497efac671
regmysss <kabanigor3@gmail.com>
2025-01-25 17:25:19 +0200

    feat: Add About component and integrate it into the App; update styles for About and ProjectBox components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
+import { About } from "./components/About";
 
 function App() {
   const { theme, setTheme } = useTheme();
@@ -13,6 +14,7 @@ function App() {
       <Home />
       <Projects />
       <Skills />
+      <About />
     </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import { InfoSection } from "./UI/InfoSection/InfoSection";
+import '../styles/about.css';
+
+export const About = () => {
+    return (
+        <section className='about'>
+            <div className='about-content'>
+                <InfoSection
+                    title={"Who I Am"}
+                    description={"Here I will tell you a little about myself."}
+                />
+                <div className='about-info'>
+                    <div className='about-text'>
+                        <p >
+                            I’m <b>Vladyslav Yarmolyuk,</b> a <b>passionate web developer</b> from Kyiv, Ukraine,
+                            with over <b>3 years of experience</b> studying and practicing modern web development.
+                            I focus on building <b>responsive, functional, and user-friendly</b> websites that
+                            solve real-world problems.
+                        </p>
+                        <br />
+                        <p >
+                            I’m always excited to explore <b>new technologies</b> and continuously learn to create
+                            better and more <b>innovative IT solutions</b>. My goal is to deliver <b>high-quality digital
+                            products</b> that not only meet but exceed user expectations.
+                        </p>
+                    </div>
+                    <div className='about-photo'>
+                        <img src="avatar.jpg" alt="photo" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+};
