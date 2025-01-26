207baf6f80502ea2f43c11eeb2e9364e2163f840
regmysss <kabanigor3@gmail.com>
2025-01-26 22:45:51 +0200

    style: Update styles for InfoSection, ProjectBox, About, Footer, Header, and Projects components; enhance responsiveness and layout

--- src/components/UI/ProjectBox/ProjectBox.jsx
+++ src/components/UI/ProjectBox/ProjectBox.jsx
@@ -4,8 +4,8 @@ export const ProjectBox = ({ title, image, link }) => {
     return (
         <div className="project-box">
             <a href={link}>
-                <div className="project-box-image-container">
-                    <img className="project-box-image" src={image} alt={title} />
+                <div className="project-box-image">
+                    <img src={image} alt={title} />
                 </div>
                 <h3>{title}</h3>
             </a>
