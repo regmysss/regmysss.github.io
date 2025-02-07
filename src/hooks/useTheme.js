0fc7134c67cab26471485bc6158cdec99207b70d
regmysss <kabanigor3@gmail.com>
2025-02-07 15:02:08 +0200

    refactor: Update useTheme hook to initialize theme based on user's color scheme preference

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -2,11 +2,17 @@ import { useLayoutEffect } from "react";
 import { useState } from "react";
 
 export const useTheme = () => {
-    const [theme, setTheme] = useState('dark');
+    const [theme, setTheme] = useState(null);
+
+    useLayoutEffect(() => {
+        const matchTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+
+        setTheme(matchTheme);
+    }, []);
 
     useLayoutEffect(() => {
         document.documentElement.setAttribute('data-theme', theme);
-    });
+    }, [theme]);
 
     return { theme, setTheme };
 }
