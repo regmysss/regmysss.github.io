77adfe491cc683a52d9577e363bfcabc09ad3825
regmysss <kabanigor3@gmail.com>
2025-06-14 15:50:21 +0300

    fix: Refactor theme initialization logic in useTheme hook for improved clarity and functionality

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -2,23 +2,18 @@ import { useLayoutEffect } from "react";
 import { useState } from "react";
 
 export const useTheme = () => {
-    const [theme, setTheme] = useState(null);
+    const [theme, setTheme] = useState(() => {
+        const storedTheme = localStorage.getItem('theme');
+        if (storedTheme)
+            return storedTheme;
+
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    });
 
     function toggleTheme(newTheme) {
         setTheme(newTheme);
         localStorage.setItem('theme', newTheme);
     }
-
-    useLayoutEffect(() => {
-        const storedTheme = localStorage.getItem('theme');
-        if (storedTheme) {
-            toggleTheme(storedTheme);
-            return;
-        }
-
-        const matchTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-        toggleTheme(matchTheme);
-    }, []);
 
     useLayoutEffect(() => {
         document.documentElement.setAttribute('data-theme', theme);
